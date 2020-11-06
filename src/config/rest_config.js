dfec9268185636691fb25d31ba13787d22fd37b0
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2020-11-06 22:53:53 +0200

    Update rest_config.js

--- src/config/rest_config.js
+++ src/config/rest_config.js
@@ -1,5 +1,5 @@
 export const base_url = 'https://api.happi.dev/v1/music';
-export const API_KEY = 'eac681LrAn1DezuIAapqKg80NaIsTOhJU5Pja1EqslrguRQxITzFzr10';
+export const API_KEY = '';
 export const tracks_url = '/bpm/playlist/80-90?limit=30';
 export const artists_url = '';
 export const albums_url = '';
